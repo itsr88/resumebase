9031b53e31431fa4dc427b3c309523d146d50d03
Andrey Rusyaykin <itsr88@gmail.com>
2022-06-29 19:43:38 +0300

    init project

---
+++ src/ArrayStorage.java
@@ -0,0 +1,53 @@
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+
+    private int size = 0;
+    Resume[] storage = new Resume[10000];
+
+    void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
+    }
+
+    void save(Resume r) {
+        storage[size] = r;
+        size++;
+    }
+
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid))
+                return storage[i];
+        }
+        return null;
+    }
+
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, size - i);
+                size--;
+                break;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        Resume[] resumeWithoutNull = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resumeWithoutNull[i] = storage[i];
+        }
+        return resumeWithoutNull;
+    }
+
+    int size() {
+        return size;
+    }
+}

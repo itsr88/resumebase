2030e1a4a39880c107b2839a8236ab5386031d19
Andrey Rusyaykin <itsr88@gmail.com>
2022-08-24 13:38:19 +0300

    update AbstractStorageTest

--- src/su/arv/webapp/storage/AbstractFileStorage.java
+++ src/su/arv/webapp/storage/AbstractFileStorage.java
@@ -76,7 +76,7 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
     protected List<Resume> doCopyAll() {
         File[] files = directory.listFiles();
         if (files == null) {
-            throw new StorageException("Directory read error", null);
+            throw new StorageException("Directory read error");
         }
         List<Resume> list = new ArrayList<>(files.length);
         for (File file : files) {
@@ -99,7 +99,7 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
     public int size() {
         String[] list = directory.list();
         if (list == null) {
-            throw new StorageException("Directory read error", null);
+            throw new StorageException("Directory read error");
         }
         return list.length;
     }
---
+++ test/su/arv/webapp/storage/ObjectStreamPathStorageTest.java
@@ -0,0 +1,8 @@
+package su.arv.webapp.storage;
+
+public class ObjectStreamPathStorageTest extends AbstractStorageTest {
+
+    public ObjectStreamPathStorageTest() {
+        super(new ObjectStreamPathStorage(STORAGE_DIR.getAbsolutePath()));
+    }
+}
